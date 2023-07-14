c60cd178071782e84355ac41e34324ad4ceb10d7
Rika Gorn <rikagx@gmail.com>
2023-07-14 09:59:07 -0400

    shell script again

---
+++ materials/Lab_Deploy_SSO_Docker/shiny_server_oss.sh
@@ -0,0 +1,20 @@
+docker build -t my-app . --platform linux/amd64
+
+docker run -it -d -p 3838:3838 --platform linux/amd64 \
+-v ./logs:/var/log/shiny-server \
+-v ./conf:/etc/shiny-server \
+my-app 
+
+# identify container information and processes runnng
+docker ps
+# exec into container
+docker exec -it {container} /bin/bash
+# list imagess
+docker image ls
+
+
+
+# runs on http://localhost:3838
+# remove platform option if running on intel instead of M1
+# check architecture by running arch
+# -p 3838:3838 maps your local port 3838, to the same port inside the container where shiny server is listening.
