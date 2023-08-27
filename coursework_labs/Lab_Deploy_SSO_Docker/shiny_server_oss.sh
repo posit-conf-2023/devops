3b6b763e1953ca65ce0d90c325a05d51a80b4428
Rika Gorn <rikagx@gmail.com>
2023-08-27 12:50:38 -0400

    startup script

--- coursework_labs/Lab_Deploy_SSO_Docker/shiny_server_oss.sh
+++ coursework_labs/Lab_Deploy_SSO_Docker/shiny_server_oss.sh
@@ -1,9 +1,19 @@
 docker build -t my-app . --platform linux/amd64
 
-docker run -it -d -p 3838:3838 --platform linux/amd64 \
+docker run --rm -p 3838:3838 --platform linux/amd64 \
+-v ./apps:/srv/shinyapps \
+-v ./logs:/var/log/shiny-server \doc
+-v ./conf:/etc/shiny-server \
+my-app 
+
+# visit http://localhost:3838 to see the app
+
+# in detached mode
+docker run -d -p 3838:3838 --platform linux/amd64 \
 -v ./logs:/var/log/shiny-server \
 -v ./conf:/etc/shiny-server \
 my-app 
+
 
 # identify container information and processes runnng
 docker ps
